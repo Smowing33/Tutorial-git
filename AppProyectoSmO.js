19bd04d6f647167bdc7e81988f2ec11be8a297ea
nicolas <nlitwinchuk@w3itsolutions.net>
2021-08-10 11:34:18 -0300

    Variables

--- AppProyectoSmO.js
+++ AppProyectoSmO.js
@@ -1,1 +1,6 @@
-document.write("<h1>Primeros pasos</h1>");
+// aca estoy hice esto para darme cuenta cuando una constante no se puede cambiar 
+//y como hacer para cambiarla: prime player es constante entonces usno jugadoresprime para mover los resultoados al momento del console log
+var jugadores = ['shadow', 'kintel33', 'vitiligun', 'osario', 'smowing']
+var jugadoresprime = ['vitiligun', 'osario', 'smowing']
+const primeplayer = jugadoresprime
+console.log(jugadores, primeplayer)
